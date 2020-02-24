0c64fa40c07d548c5ac704452a2ecd600a46fd33
Alexander Kiranov <xhaskx@gmail.com>
2020-02-24 10:03:47 +0300

    added nodejs mqtt client example

---
+++ mqtt-client/README.md
@@ -0,0 +1,31 @@
+# MQTT Client examples
+
+All provided examples are performing the simple but complete MQTT client workflow:
+
+* connect to mqtt broker
+* subscribe to "test" topic
+* publish a message to the "test" topic
+* receive published message in a separate callback
+* disconnect from the mqtt broker
+
+## Requirements
+
+To connect to [flespi mqtt broker](https://flespi.com/mqtt-broker) you have to obtain a flespi token.
+
+Please [refer here](https://flespi.com/kb/tokens-access-keys-to-flespi-platform) to read how to do it (don't worry, it's fast and simple).
+
+## nodejs
+
+Example [is here](./nodejs/)
+
+To run the node.js example you need to install the dependencies (mqtt package):
+
+```sh
+npm install
+```
+
+And then run the entry point script with FlespiToken passed through the process environment:
+
+```sh
+FlespiToken=XXXXXXXXXXXXXXX npm run main # runs node ./index.js
+```
---
+++ mqtt-client/nodejs/index.js
@@ -0,0 +1,36 @@
+const mqtt = require('mqtt');
+
+const client  = mqtt.connect('mqtts://mqtt.flespi.io:8883', {
+  // see https://flespi.com/kb/tokens-access-keys-to-flespi-platform to read about flespi tokens
+  username: `FlespiToken ${process.env.FlespiToken}`,
+  protocolVersion: 5,
+  clean: true,
+});
+console.log('mqtt client created, connecting...');
+
+client.on('connect', () => {
+  console.log('connected, subscribing to "test" topic...');
+
+  client.subscribe('test', (err) => {
+    if (err) {
+      console.log('failed to subscribe to topic "test":', err);
+      return;
+    }
+    console.log('subscribed to "test" topic, publishing message...');
+    client.publish('test', 'hello from flespi nodejs mqtt client example script!', {qos: 1});
+  });
+});
+
+client.on('message', (topic, msg) => {
+  console.log(`received message in topic "${topic}": "${msg.toString('utf8')}"`);
+  console.log('disconnecting...');
+  client.end();
+});
+
+client.on('close', () => {
+  console.log('disconnected');
+})
+
+client.on('error', (err) => {
+  console.log('mqtt client error:', err);
+});
